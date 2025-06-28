af32afc7dd88de82dc7e4e6ac7837f0625e23939
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-28 23:17:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
+app.use(cors({ 
   origin: [
     'http://localhost:5173',
     'https://airbridge-gamma.vercel.app'
