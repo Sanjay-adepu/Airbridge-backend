aebaae3f12f5c2bc49fe48222ca36dadd0d52a92
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-29 11:38:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://airbridge-gamma.vercel.app',
+  ],
+};
+
+app.use(cors(corsOptions));
+
+
+
 app.use(express.json());
 
 const sessions = {};
